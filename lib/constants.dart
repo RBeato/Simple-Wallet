c8a51f65fd7dbd14425f120cf14f10982320f785
Romeu Beato <romeufbeato@gmail.com>
2021-10-20 14:53:47 +0100

    missing error handling and event listening

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,6 +1,6 @@
 class Constants {
   static const String getBalanceAmount = 'getBalanceAmount';
   static const String getDepositAmount = 'getDepositAmount';
-  static const String addDepositAmount = 'addDepositAmount value';
+  static const String addDepositAmount = 'addDepositAmount';
   static const String withdrawAmount = 'withdrawAmount';
 }
